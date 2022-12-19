c1d98f92cc25f1412f4b2f81dd98cfd32b412804
Molly Mlynarczyk <mlynarczyk.molly@principal.com>
2022-12-19 15:39:17 -0600

    Day 4, Pt 2

--- day4/cleanup.py
+++ day4/cleanup.py
@@ -1,14 +1,12 @@
 
-with open("test.txt") as input:
+with open("input.txt") as input:
     worst_pairs = 0
     for line in input.readlines():
         line = line.strip()
         sections = line.split(",")
         elf1_nums = sections[0].split("-")
         elf2_nums = sections[1].split("-")
-        #check first against second
-        if int(elf1_nums[0]) >= int(elf2_nums[0]) and int(elf1_nums[1]) <= int(elf2_nums[1]):
+        if (not int(elf1_nums[1]) < int(elf2_nums[0])) and (not int(elf2_nums[1]) < int(elf1_nums[0])):
+            print(line + " Overlap")
             worst_pairs += 1
-        elif int(elf2_nums[0]) >= int(elf1_nums[0]) and int(elf2_nums[1]) <= int(elf1_nums[1]):
-            worst_pairs += 1
-    print(worst_pairs)    
+    print(worst_pairs) 
